8a24136c3c2af5864d735815c3d88f98c726e390
lpgoulart <leonardo@roffor.com.br>
2019-10-31 11:25:02 -0300

    finish implementation of edit component

--- src/app/tasks/edit/edit.component.ts
+++ src/app/tasks/edit/edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { NgForm } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { Task, TaskService } from "../shared";
+
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild ( 'formTask', {static: false} ) formTask: NgForm;
+  task: Task;
 
-  ngOnInit() {
+  constructor(
+    private taskService: TaskService,
+    private route: Router,
+    private router: ActivatedRoute
+  ) { }
+
+  ngOnInit() { 
+    const id = +this.router.snapshot.params['id'];
+    this.task = this.taskService.searchById(id) 
+  }
+
+  update(): void {
+    if ( this.formTask.form.valid ) {
+      this.taskService.update(this.task);
+      this.route.navigate(["/tasks"])
+    }
   }
 
 }
