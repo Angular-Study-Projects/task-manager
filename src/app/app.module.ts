bd6c39af812ee7621c82eda4f4c1eb6e3c624110
lpgoulart <leonardo@roffor.com.br>
2019-10-31 08:28:23 -0300

    upload project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TasksModule } from "./tasks";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TasksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
